3d175f8d98c4c7d14cebb5d7bf2aa2241b0784e6
mackemo <mackenzielmoore14@gmail.com>
2024-04-17 17:17:43 -0400

    code

---
+++ script.js
@@ -0,0 +1,52 @@
+const searchBtn = document.getElementById('search-btn');
+const apiKey = `GSR7zoBZ7gl3FAdkEk2rHsu7C85lXVHL`;
+const city = JSON.parse(localStorage.getItem('search-input')) || [];
+
+
+searchBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let searchInput = document.getElementById('search-input').value;
+    city.push(searchInput);
+    localStorage.setItem('search-input', JSON.stringify(city));
+
+    searchAPI(city);
+    
+});
+
+function searchAPI(city) {
+
+    const geoURL = `https://api.tomtom.com/search/2/geocode/${city}.json?key=${apiKey}`;
+
+    fetch(geoURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (geoResults) {
+            const lat = geoResults.results[0].position.lat;
+            const lon = geoResults.results[0].position.lon;
+            console.log(lat);
+            console.log(lon);
+
+            const apiUrl = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?key=${apiKey}&point=${lat},${lon}`;
+
+            fetch(apiUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (apiData) {
+                    console.log(apiData);
+                })
+
+            const ocURL = `https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=100&key=789f86d1-a5b2-4530-8ca0-fa64aebcc952&point=${lat},${lon}`;
+    
+            fetch(ocURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (ocData) {
+                    console.log(ocData);
+                })
+        })
+}
+
